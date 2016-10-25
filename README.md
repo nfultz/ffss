cafd801aec1355a26401f394a72148356f4befef
Neal Fultz <nfultz@gmail.com>
2016-10-25 23:39:08 +0000

    Adding shell script, packaging metadata

---
+++ README.md
@@ -0,0 +1,14 @@
+#ffss - Pager for Flat Files and Tables
+
+ffss is a pager in the tradition of `more` or `less`, specifically for flat files and tables. 
+
+It sports `vim` inspired keybindings for navigation, and is `screen`-aware.
+
+With a little 
+
+##TODO
+
+1. `tmux` awareness
+2. 
+
+This would be cleaner as a custom exporter for nbconvert.
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='ffss',
+      version='0.1',
+      description='Flat File Pager',
+      url='http://github.com/nfultz/ffss',
+      download_url="https://github.com/nfultz/ffss/tarball/0.1",
+      author='Neal Fultz',
+      author_email='nfultz@gmail.com',
+      license='BSD',
+      packages=['ffss'],
+      install_requires=['ncurses'],
+      zip_safe=False,
+      include_package_data=True,
+      scripts=['bin/ffss']
+)
